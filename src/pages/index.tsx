60a9e600d073ea1677fab6552fc052b251cab4c4
bangdler <zbthz90@gmail.com>
2023-03-10 18:09:29 +0900

    refactor: 잘못된 import/export 컴포넌트 삭제 [TRL-141]

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
-import Layout from '@/components/Layout';
 import Home from '@/pages/Home';
 import Login from '@/pages/Login';
 import NotFound from '@/pages/NotFound';
 import User from '@/pages/User';
 
-export { Layout, Home, Login, NotFound, User };
+export { Home, Login, NotFound, User };

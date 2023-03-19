3cffaf87efe8566ea9bf969774b5d5fd4f6ba6e6
bangdler <zbthz90@gmail.com>
2023-03-19 17:16:25 +0900

    refactor: login svg 적용을 위해 OAuthBtn props에 svg 추가 [TRL-96]

--- src/components/OAuthBtnContainer/index.tsx
+++ src/components/OAuthBtnContainer/index.tsx
@@ -1,20 +1,34 @@
 import { useQueryErrorResetBoundary } from '@tanstack/react-query';
 import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+import styled from 'styled-components';
 
+import { ReactComponent as GoogleLogin } from '@/assets/google-login.svg';
+import { ReactComponent as KakaoLogin } from '@/assets/kakao-login.svg';
+import { ReactComponent as NaverLogin } from '@/assets/naver-login.svg';
 import OAuthBtn from '@/components/OAuthBtn';
 import ErrorFallback from '@/components/OAuthBtnContainer/errorFallback';
 import LoadingFallback from '@/components/OAuthBtnContainer/loadingFallback';
 
 const OAuthBtnContainer = () => {
-  const OAuthList = ['google', 'naver', 'kakao'];
+  const OAuthList = [
+    { name: 'google', svg: <GoogleLogin /> },
+    {
+      name: 'naver',
+      svg: <NaverLogin />,
+    },
+    { name: 'kakao', svg: <KakaoLogin /> },
+  ];
 
   const { reset } = useQueryErrorResetBoundary();
 
   const OAuthBtnList = OAuthList.map(oauthServer => {
     return (
-      <li key={oauthServer}>
-        <OAuthBtn oauthServer={oauthServer} />
+      <li key={oauthServer.name}>
+        <OAuthBtn
+          oauthServerName={oauthServer.name}
+          oauthServerSvg={oauthServer.svg}
+        />
       </li>
     );
   });

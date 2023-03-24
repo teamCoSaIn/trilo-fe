a8657f08db40fd34581b9f9ebf0bbe3a994a2e20
bangdler <zbthz90@gmail.com>
2023-03-24 12:19:01 +0900

    feat: App 진입 시 1회 AccessToken 갱신 및 userProfile 요청 로직 구현 [TRL-139]

--- src/components/AuthProvider/index.tsx
+++ src/components/AuthProvider/index.tsx
@@ -12,28 +12,41 @@ interface AuthProviderProps {
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
   const setUserStatus = useSetRecoilState(UserStatus);
-
   const setUserProfileNickname = useSetRecoilState(UserProfileNickname);
   const setUserProfileImgUrl = useSetRecoilState(UserProfileImgUrl);
 
-  useQuery(['checkRefresh'], () => HTTP.checkRefreshToken(), {
-    onSuccess: async response => {
-      if (response.response) {
-        const { nickname, imgUrl } = await HTTP.getUserProfile();
-        if (nickname) {
-          setUserProfileNickname(nickname);
+  const { data } = useQuery(
+    ['checkRefreshToken'],
+    () => HTTP.checkRefreshToken(),
+    {
+      onSuccess: async response => {
+        if (response.response) {
+          return setUserStatus(UserStatusTypes.LOGIN);
         }
-        if (imgUrl) {
-          setUserProfileImgUrl(imgUrl);
-        }
-        return setUserStatus(UserStatusTypes.LOGIN);
+        return setUserStatus(UserStatusTypes.LOGOUT);
+      },
+      staleTime: 30 * 60 * 1000,
+      refetchOnWindowFocus: true,
+      refetchInterval: 30 * 60 * 1000,
+      suspense: true,
+      useErrorBoundary: false,
+    }
+  );
+
+  useQuery(['setLogin'], () => HTTP.refreshAccessToken(), {
+    onSuccess: async () => {
+      const { nickname, imgUrl } = await HTTP.getUserProfile();
+      if (nickname) {
+        setUserProfileNickname(nickname);
       }
-      return setUserStatus(UserStatusTypes.LOGOUT);
+      if (imgUrl) {
+        setUserProfileImgUrl(imgUrl);
+      }
     },
-    staleTime: 30 * 60 * 1000,
-    refetchInterval: 30 * 60 * 1000,
-    refetchOnWindowFocus: true,
+    enabled: !!data?.response,
+    staleTime: Infinity,
     suspense: true,
+    useErrorBoundary: false,
   });
 
   return <>{children}</>;

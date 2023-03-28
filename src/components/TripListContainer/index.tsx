3d79c9227e2e049333cfe2a4f6c121c17d0b45dd
bangdler <zbthz90@gmail.com>
2023-03-28 17:52:05 +0900

    feat: trip list 페이지에서 trip list 요청 로직 구현 [TRL-78]

---
+++ src/components/TripListContainer/index.tsx
@@ -0,0 +1,78 @@
+import { useQuery } from '@tanstack/react-query';
+import { useRecoilValue } from 'recoil';
+import styled from 'styled-components';
+
+import HTTP from '@/api';
+import Description from '@/components/common/Description';
+import TripCard from '@/components/TripCard';
+import color from '@/constants/color';
+import { UserProfileNickname } from '@/states/userProfile';
+
+const TripListContainer = () => {
+  // TODO: 방문자일 때와 로그인일 때 구분
+  const nickname = useRecoilValue(UserProfileNickname);
+
+  const { data: tripCardData } = useQuery(
+    ['tripList'],
+    () => HTTP.getTripList(),
+    {
+      suspense: true,
+    }
+  );
+
+  const TripCards = tripCardData?.map(cardData => {
+    return <TripCard cardData={cardData} />;
+  });
+
+  return (
+    <>
+      <DescriptionBox>
+        <Wrapper>
+          <Description color={color.blue3} fontSize={2.4}>
+            {nickname}님의 여행기록
+          </Description>
+          <Label>{tripCardData?.length}개</Label>
+        </Wrapper>
+        <Description color="#979696">
+          트릴로와 함께 즐거운 여행을 시작해보세요.
+        </Description>
+      </DescriptionBox>
+      <TripCardsBox>{TripCards}</TripCardsBox>
+    </>
+  );
+};
+
+const DescriptionBox = styled.div`
+  min-width: 600px;
+  display: flex;
+  flex-direction: column;
+  gap: 13px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 14px;
+`;
+
+// TODO: common 분리
+const Label = styled.div`
+  width: 52px;
+  height: 23px;
+  border-radius: 15px;
+  background-color: #4d77ff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 1.4rem;
+`;
+
+const TripCardsBox = styled.div`
+  min-width: 600px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 48px;
+  margin-top: 47px;
+`;
+
+export default TripListContainer;
--- src/pages/TripList/index.tsx
+++ src/pages/TripList/index.tsx
@@ -1,5 +1,34 @@
+import { useQueryErrorResetBoundary } from '@tanstack/react-query';
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import styled from 'styled-components';
+
+import Error from '@/components/common/Error';
+import CircularLoader from '@/components/common/Loader';
+import TripListContainer from '@/components/TripListContainer';
+
 const TripList = () => {
-  return <div>TripList Page 입니다.</div>;
+  const { reset } = useQueryErrorResetBoundary();
+
+  return (
+    <Layout>
+      <ErrorBoundary FallbackComponent={Error} onReset={reset}>
+        <Suspense fallback={<CircularLoader />}>
+          <TripListContainer />
+        </Suspense>
+      </ErrorBoundary>
+    </Layout>
+  );
 };
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  padding-top: 104px;
+  margin: 0 auto;
+`;
 
 export default TripList;

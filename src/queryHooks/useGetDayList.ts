0bf3db46ab5c2fe5ed3bf6ff8e4f88d71f4aa626
jthw1005 <skl0105@naver.com>
2023-04-17 16:12:01 +0900

    refactor: dayList 요청 API 옵션 값 수정 [TRL-178]

--- src/queryHooks/useGetDayList.ts
+++ src/queryHooks/useGetDayList.ts
@@ -1,9 +1,22 @@
 import { useQuery } from '@tanstack/react-query';
 
 import HTTP from '@/api';
+import { PlanDay } from '@/api/planDay';
 
-const useGetDayList = (planId: string) => {
-  return useQuery(['dayList'], () => HTTP.getPlanDayList(planId));
+interface UseGetDayListParam {
+  planId: string;
+  onSuccess?: (data: PlanDay[]) => void;
+  onError?: () => void;
+}
+
+const useGetDayList = ({ planId, onSuccess, onError }: UseGetDayListParam) => {
+  return useQuery([`dayList${planId}`], () => HTTP.getPlanDayList(planId), {
+    onSuccess,
+    onError,
+    suspense: true,
+    staleTime: Infinity,
+    cacheTime: 1000 * 60 * 10,
+  });
 };
 
 export default useGetDayList;

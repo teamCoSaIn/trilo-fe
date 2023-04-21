9ce89416eea7b99c6b7bdc334d63c2ae3a29c5f2
jthw1005 <skl0105@naver.com>
2023-04-21 16:04:09 +0900

    design: PlaceCard UI 구현 [TRL-192]

---
+++ src/components/PlaceSearchList/PlaceCard/index.tsx
@@ -0,0 +1,153 @@
+import styled from 'styled-components';
+
+import { ReactComponent as CopyIcon } from '@/assets/copy.svg';
+import { ReactComponent as GoogleIcon } from '@/assets/google.svg';
+import { ReactComponent as NaverIcon } from '@/assets/naver.svg';
+import Logo from '@/components/common/Logo';
+import PlaceCardStar from '@/components/PlaceSearchList/PlaceCardStar';
+
+interface PlaceCardProps {
+  name: string | undefined;
+  rating: number | undefined;
+  address: string | undefined;
+  numOfReviews: number | undefined;
+  openingHours: google.maps.places.PlaceOpeningHours | undefined;
+  imgUrl: string | null;
+}
+
+const PlaceCard = ({
+  name,
+  rating,
+  address,
+  numOfReviews,
+  openingHours,
+  imgUrl,
+}: PlaceCardProps) => {
+  return (
+    <PlaceCardBox>
+      <PlaceCardContent>
+        <PlaceCardTitle>{name}</PlaceCardTitle>
+        <PlaceCardRatingBox>
+          <PlaceCardRating>{rating?.toFixed(1)}</PlaceCardRating>
+          <PlaceCardStar rating={rating} />
+          <PlaceCardNumOfReviews>({numOfReviews})</PlaceCardNumOfReviews>
+        </PlaceCardRatingBox>
+        <PlaceCardLinkBtnBox>
+          <PlaceCardGoogleLinkBtn
+            onClick={() => {
+              window.open('https://www.google.com');
+            }}
+          >
+            <GoogleIcon />
+            구글 맵
+          </PlaceCardGoogleLinkBtn>
+          <PlaceCardGoogleLinkBtn
+            onClick={() => {
+              window.open('https://www.naver.com');
+            }}
+          >
+            <NaverIcon />
+            네이버
+          </PlaceCardGoogleLinkBtn>
+        </PlaceCardLinkBtnBox>
+      </PlaceCardContent>
+      {imgUrl ? (
+        <PlaceCardImg src={imgUrl} />
+      ) : (
+        <Logo width={120} height={120} />
+      )}
+    </PlaceCardBox>
+  );
+};
+
+const PlaceCardBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 337px;
+  height: 160px;
+  background: #ffffff;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 7px;
+  padding: 20px;
+`;
+
+const PlaceCardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const PlaceCardTitle = styled.h2`
+  font-size: 14px;
+  font-weight: 700;
+`;
+
+const PlaceCardRatingBox = styled.div`
+  display: flex;
+  gap: 6px;
+  align-items: center;
+  margin-top: 10px;
+`;
+
+const PlaceCardRating = styled.span`
+  font-size: 12px;
+  font-weight: 700;
+  color: #4f4f4f;
+`;
+
+const PlaceCardNumOfReviews = styled.span`
+  font-size: 12px;
+  font-weight: 400;
+  color: #4f4f4f;
+`;
+
+const PlaceCardAddressBtn = styled.button`
+  display: flex;
+  gap: 5px;
+  font-size: 12px;
+  font-weight: 400;
+  margin-top: 15px;
+`;
+
+const PlaceCardBusinessHoursBox = styled.div`
+  display: flex;
+  gap: 5px;
+  margin-top: 12px;
+  font-size: 12px;
+  font-weight: 500;
+  color: #4f4f4f;
+`;
+
+const PlaceCardBusinessHours = styled.span`
+  font-size: 12px;
+  font-weight: 400;
+  color: inherit;
+`;
+
+const PlaceCardLinkBtnBox = styled.div`
+  display: flex;
+  gap: 6px;
+  margin-top: 11px;
+`;
+
+const PlaceCardGoogleLinkBtn = styled.button`
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  justify-content: center;
+  width: 59px;
+  height: 18px;
+  background: #fff;
+  box-shadow: 0px 2px 10px 1px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  font-size: 8px;
+  font-weight: 400;
+`;
+
+const PlaceCardImg = styled.img`
+  width: 120px;
+  height: 120px;
+  border-radius: 7px;
+`;
+
+export default PlaceCard;

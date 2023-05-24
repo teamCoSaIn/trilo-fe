b702bdd7f120b31304aca924448e6da50e8ced59
bangdler <zbthz90@gmail.com>
2023-05-24 16:01:36 +0900

    feat: getTripList API pagination 반영 및 더보기 기능 구현 [TRL-276]

--- src/api/trip.ts
+++ src/api/trip.ts
@@ -17,11 +17,27 @@ export type TCreateTripTitleParams = ITrip['title'];
 
 export type TDeleteTripTitleParams = ITrip['tripId'];
 
-export const getTripList = async () => {
-  const res = await axios<ITrip[]>({
+interface IGetTripListParams {
+  tripperId: number;
+  size?: number;
+  page?: number;
+  sort?: string;
+}
+
+interface IGetTripListResponse {
+  trips: ITrip[];
+  totalCount: number;
+  totalPage: number;
+  currentPage: number;
+  isLast: boolean;
+}
+
+export const getTripList = async (reqParams: IGetTripListParams) => {
+  const res = await axios<IGetTripListResponse>({
     method: 'get',
     url: `/tripcard-list`,
     requireAuth: true,
+    params: reqParams,
   });
   return res.data;
 };

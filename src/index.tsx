4a675e77fcbb87cc73173449be8b92a8e3b89ca1
jthw1005 <skl0105@naver.com>
2023-07-04 15:18:39 +0900

    build: toastify 설치 [TRL-383]

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
 import { RecoilRoot } from 'recoil';
 
 import App from '@/App';
 import GlobalStyle from '@/styles/GlobalStyle';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 

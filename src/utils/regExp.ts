c3a971d97b2642a5188725cf8d0d9a4155df730c
jthw1005 <skl0105@naver.com>
2023-07-14 17:12:49 +0900

    refactor: 닉네임 수정 시 길이 제한 변경 [TRL-429]

--- src/utils/regExp.ts
+++ src/utils/regExp.ts
@@ -1,7 +1,17 @@
-const nicknameRegExp = /^[ㄱ-ㅎ가-힣a-zA-Z0-9]{3,20}$/;
+const nicknameRegExp = /^[ㄱ-ㅎ가-힣a-zA-Z0-9]{1,100}$/;
 
 const placeSearchInputRegExp = /^[^<>]{1,85}$/;
 
 const tripTitleRegExp = /^[^<>]{1,20}$/;
 
-export { nicknameRegExp, placeSearchInputRegExp, tripTitleRegExp };
+const specialCharRegExp = /[^\w\s]/;
+
+const min1max100RegExp = /^.{1,100}$/;
+
+export {
+  nicknameRegExp,
+  placeSearchInputRegExp,
+  tripTitleRegExp,
+  specialCharRegExp,
+  min1max100RegExp,
+};
